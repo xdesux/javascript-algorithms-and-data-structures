67072472f159deb85f7669956df8257d7d503639
xdesux <erickguillano12@gmail.com>
2023-09-18 08:43:19 +0800

    yawa

--- Regular Expressions/Extract_Matches.js
+++ Regular Expressions/Extract_Matches.js
@@ -2,4 +2,6 @@ let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
 let result = extractStr.match(codingRegex); // Change this line
 
-/**dsvfjsvfjhsshdfsd  */
+/**dsvfjsvfjhsshdfsd 
+ * yawa mani
+ */
