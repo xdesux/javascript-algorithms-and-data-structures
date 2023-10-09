85913e4d1ed09572bb83006d214e5e4d3a20f518
xdesux <erickguillano12@gmail.com>
2023-10-09 21:14:32 +0800

    herh

---
+++ Basic Algorithm Scripting/Factorialize a Number.js
@@ -0,0 +1,22 @@
+/**Factorialize a Number
+Return the factorial of the provided integer.
+
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Only integers greater than or equal to zero will be supplied to the function. */
+
+function factorialize(num) {
+    let factorial = 1;
+    for(let i= 2; i<=num; i++){
+      
+      factorial *= i;  
+    }
+    return factorial;
+  
+  }
+  
+  factorialize(5);
---
+++ Basic Algorithm Scripting/Reverse a String.js
@@ -0,0 +1,12 @@
+/**
+ * Reverse a String
+Reverse the provided string and return the reversed string.
+
+For example, "hello" should become "olleh".
+ */
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+  }
+  
+  reverseString("hello");
